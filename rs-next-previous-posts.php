6640d55192d843987271e0b864a57cb46cd2907c
Radley <radleygh@gmail.com>
2018-05-12 16:00:13 -0700

    Added license URI

--- rs-next-previous-posts.php
+++ rs-next-previous-posts.php
@@ -6,6 +6,7 @@ Author: Radley Sustaire
 Author URI: https://radleysustaire.com/
 Version: 1.0.4
 License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 */
 
 define( 'RSNPP_VERSION', '1.0.4' );
